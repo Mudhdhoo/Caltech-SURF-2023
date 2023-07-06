5e07af9c369a37f8c08287aa8e33a6651794a6e6
John Cao <johncao2001@gmail.com>
2023-07-05 17:13:29 -0700

    Finished uupdate_MBO

--- Segmenter.py
+++ Segmenter.py
@@ -3,7 +3,8 @@ from Image import Image
 import matplotlib.pyplot as plt
 
 class Segmenter:
-    def __init__(self, image: Image, delta, GL_epsilon, steps, margin_proportion, maxiterations, grad_Bhatt_MC, Bhatt_MC, beta, gamma, momentum_u) -> None:
+    def __init__(self, image: Image, delta, GL_epsilon, steps, margin_proportion, maxiterations, 
+                grad_Bhatt_MC, Bhatt_MC, beta, gamma, momentum_u, threshold_seg, max_sparsity_seg, batch_size) -> None:
         self.image = image   # The image to segment
 
         # Segmentation Parameters
@@ -20,6 +21,11 @@ class Segmenter:
         self.beta = beta
         self.gamma = gamma
         self.momentum_u = momentum_u
+
+        # Parameters for computing P1, P2
+        self.threshold_seg = threshold_seg
+        self.max_sparsity_seg  = max_sparsity_seg # 1e7; %Maximum entries in sparse matrix
+        self.batch_size = batch_size # size of batches for batch processing
 
     def __init_u0(self):
         """
@@ -40,8 +46,7 @@ class Segmenter:
 
     def __uupdate_MBO(self, verbose = False):
         """
-        Implements the MBO scheme 
-        Im,u0,beta,gamma,epsilon,momentum_u,M1,N1,KER,delta,margin_proportion,image,movie_filename,threshold,sigma,grad_Bhatt_MC,Bhatt_MC,max_sparsity,batch_size,maxiterations,method,steps,groundtruth,verbose
+        Implements the modified MBO scheme 
         """
         J = self.image.J
         u = self.u0
@@ -50,6 +55,31 @@ class Segmenter:
         b = 2*self.momentum_u #     parameters in the diffusion
         M1, N1 = self.image.image_size[0], self.image.image_size[1] # 2D dimension of image
         
+        # Compute eigendecomposition
+        eye_minus_adt_Lap_inv = np.linalg.inv(np.eye(M1) - a*(dt/self.steps)*self.__Lap1D_neu(M1))
+        eye_minus_adt_Lap_inv = (eye_minus_adt_Lap_inv + eye_minus_adt_Lap_inv.T)/2
+        sigma, phi = np.linalg.eig(eye_minus_adt_Lap_inv)
+
+        # Run iterations
+        for i in range(0,self.maxiterations):
+            u_old = u
+            v = self.__force_diffuse(u,self.u0,a,b,dt,steps,eye_minus_adt_Lap_inv,phi,sigma,verbose);  # Forced diffusion
+
+            # Gradient descent step in Bhatt
+            if self.beta > 0:
+                v2 = v - dt * beta * self.__grad_Bhatt_u(J,u,indices,M1,N1,grad_Bhatt_MC,Bhatt_MC,self.threshold_seg, self.max_sparsity_seg,self.batch_size,sigma,verbose)  # Can also att "method" argument later
+            else:
+                v2 = v
+
+            # Thresholding
+            u = np.heaviside(v2 - 0.5, 0)
+
+            # Stopping condition
+            dist = np.sum(np.abs(u - u_old))
+            if dist < self.delta:
+                break
+
+        return u
 
     def __Lap1D_neu(self, M):
         """
@@ -70,6 +100,11 @@ class Segmenter:
     def __force_diffuse(self):
         pass
 
+    def __grad_Bhatt_u(J,u,indices,M1,N1,grad_Bhatt_MC,Bhatt_MC,threshold,max_sparsity,batch_size,sigma,method,verbose):
+        pass
+
+    def __margin_finder(self):
+        pass
 
 if __name__ == '__main__':
     im = Image('image')
@@ -84,6 +119,6 @@ if __name__ == '__main__':
     grad_Bhatt_MC = 10
     Bhatt_MC = 50
     seg = Segmenter(im, delta, GL_epsilon, steps, margin_proportion, maxiterations, grad_Bhatt_MC, Bhatt_MC, beta, gamma, momentum_u)
-    print(seg.Lap1D_neu(8))
+    seg.uupdate_MBO()
 
 
